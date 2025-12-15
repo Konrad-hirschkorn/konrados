7700f264fe9452b6e155708b722eee24a98d3dc6
Konrad Hirschkorn <konrad.hirschkorn@gmail.com>
2025-12-15 13:43:55 +0100

    fix(common.nix): update insecure package name for intune-portal in nixpkgs configuration

--- common/common.nix
+++ common/common.nix
@@ -55,7 +55,7 @@ in {
   nixpkgs.config.allowInsecure = [
     "terraform-1.14.0" # For coder
     "steam-1.0.0.85"
-    "intune-portal-version"
+    "intune-portal"
   ];
 
   # HIER: unsicheres Paket erlauben
